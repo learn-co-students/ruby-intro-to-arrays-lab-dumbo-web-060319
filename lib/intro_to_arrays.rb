297b834c9b1aee1305b683807ed95c8b62d490ab
Alexander Schelchere <ascellie@gmail.com>
2019-05-11 20:34:36 -0400

    Initiate new Array - complete

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+# first method
+def instantiate_new_array
+  arr = []
+end
+
+# second method
+def array_with_two_elements
+
+end
+
+# third method
+def first_element
+
+end
+
+# 4th method
+def third_element
+
+end
+# 5th method
+def last_element
+
+end
