fa6c25bca06b502cf4e181fcc010e0f9885e1560
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-25 07:54:39 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  return []
+end 
+def array_with_two_elements
+  return [1,2]
+end 
+def first_element(arg)
+  return arg[0]
+end 
+def third_element(arg)
+  return arg[2]
+end 
+def last_element(arg)
+  return arg[-1]
+end 
+def first_element_with_array_methods(arg)
+  return arg.first 
+end 
+def last_element_with_array_methods(arg)
+  return arg.last 
+end 
+def length_of_array(arg)
+  return arg.length 
+end 
